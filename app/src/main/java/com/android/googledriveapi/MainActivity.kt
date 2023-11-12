71a530d83e69d9bab89a6a81b2352b5ff4fc4a55
Nahian98 <kanon1707030@stud.kuet.ac.bd>
2023-11-12 12:19:05 +0600

    Initial commit

---
+++ app/src/main/java/com/android/googledriveapi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.googledriveapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        DriveQuickstart.main()
+    }
+}
